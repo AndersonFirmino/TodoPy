fbe0d3f68f36955e1973a4cebc348bde7cfbb6c8
Anderson Araujo <ads.csharp@gmail.com>
2016-01-20 05:30:51 -0200

    incluindo ng-Route e configurando ele

--- static/js/angular/main.js
+++ static/js/angular/main.js
@@ -1,8 +1,12 @@
 (function(){
+// angular.module('todo-py', []);
 
-  // angular.module("todo-py", ["ngRoute"]).config(function($routeProvider){
-
-  // });
-angular.module('todo-py', []);
+  angular.module("todo-py", ["ngRoute"])
+    .config(function($routeProvider){
+      $routeProvider.when('/todo', {
+        templateUrl: "/static/partials/index.html",
+        controller: "TodoController"
+      })
+    });
 
 }).call(this);
